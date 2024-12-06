b61b7d20dd2f0df3521ba103df0dac095ccb6f24
Ronaldo Chavez <chavezronaldo675@gmail.com>
2024-12-05 23:39:02 -0600

    Benjamine8

--- keys.js
+++ keys.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+
+require('dotenv').config(); // Carga las variables desde el archivo .env
+
 exports.default = {
     database: {
-        host: ' db-msi-2410-do-user-18506555-0.l.db.ondigitalocean.com', //si estuviera externa la base de datos aquí se pondría la IP
-        user: 'doadmin',
-        password: 'AVNS_BfWhBUQyau8resGOW5i',
-        database: 'defaultdb',
-        port: 25060
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'testdb',
+        port: process.env.DB_PORT || 3306
     }
 };
