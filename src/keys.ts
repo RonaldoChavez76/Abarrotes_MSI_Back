be404b73e0267c5c2c987487be75cc59dd66836f
Ronaldo Chavez <chavezronaldo675@gmail.com>
2024-12-05 20:49:35 -0600

    Benjamine2

--- src/keys.ts
+++
@@ -1,9 +0,0 @@
-export default{
-    database : {
-        host: 'localhost', //si estuviera externa la base de datos aquí se pondría la IP
-        user: 'root',
-        password: '',
-        database: 'ng_products'
-        //port: 51795
-}
-}
